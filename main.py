32812979d7d57b9b963ac59b9eca521663f05818
Hyemi <44951302+hyemi777@users.noreply.github.com>
2021-07-01 01:16:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+while True: 
+  choices = ['rock', 'paper', 'scissors']
+  computerChoice = choices[random.randint(0,2)]
+  print("This is computer choice " + computerChoice + "\n");
+  userChoice = input("What is your choice? (Rock, paper, scissors)\n")
+  if userChoice == computerChoice :
+    print("Tie! Try again.")
+    
+  elif (userChoice == "rock" and computerChoice == "paper") or (userChoice == "scissors" and computerChoice == "rock") or (userChoice == "paper" and computerChoice == "scissors"):
+    print("Computer wins!")
+
+  elif (userChoice == "rock" and computerChoice == "scissors") or (userChoice == "scissors" and computerChoice == "paper") or (userChoice == "paper" and computerChoice == "rock"):
+    print("You win!")
+  else :
+    print("I can't do that yet.")
